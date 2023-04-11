e6a39fb9b4784fa7fee5d42f1a2f9769a835bbd0
Lucas Ximenes <lucasdximenes@gmail.com>
2023-04-11 11:01:52 -0300

    feat: :rocket: implement `CompleteReport` generator

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,29 @@
-class CompleteReport:
-    pass
+from collections import defaultdict
+from .simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate_report(data):
+        stock_of_each_company = defaultdict(int)
+
+        for product in data:
+            company = product["nome_da_empresa"]
+            stock_of_each_company[company] += 1
+
+        report = "\n".join(
+            [
+                f"- {company}: {stock}"
+                for company, stock in stock_of_each_company.items()
+            ]
+        )
+
+        return report + "\n"
+
+    @staticmethod
+    def generate(data: list[dict]) -> str:
+        report = SimpleReport.generate(data)
+        report += "\n"
+        report += "Produtos estocados por empresa:\n"
+        report += CompleteReport.generate_report(data)
+        return report
