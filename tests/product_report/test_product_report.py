4d146f77877559eb37eab24aecbb28399b38dbf6
Lucas Ximenes <lucasdximenes@gmail.com>
2023-04-11 14:11:02 -0300

    test: :white_check_mark: add product report test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Sabão em pó",
+        "Omo",
+        "2023-03-13",
+        "2024-03-13",
+        "123456789",
+        "em local seco e arejado",
+    )
+    assert (
+        product.__repr__() == "O produto Sabão em pó"
+        " fabricado em 2023-03-13"
+        " por Omo com validade"
+        " até 2024-03-13"
+        " precisa ser armazenado em local seco e arejado."
+    )
